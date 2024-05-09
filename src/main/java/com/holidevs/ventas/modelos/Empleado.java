f23161ead934403d0f796e29ad783acfe72b1b20
Roberto Vetere <Roberto.vetere@gmail.com>
2024-05-09 17:40:31 +0200

    ADD Oficina Entity

--- src/main/java/com/holidevs/ventas/modelos/Empleado.java
+++ src/main/java/com/holidevs/ventas/modelos/Empleado.java
@@ -6,6 +6,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Getter
 @NoArgsConstructor
@@ -25,12 +28,44 @@ public class Empleado {
     private String email;
     private String titulacionEstudiosMaximos;
 
-    public Empleado(String nombre, String apellidos, String extension, String email, String titulacionEstudiosMaximos) {
+    @ManyToOne
+    @JoinColumn(name = "supervisor_id")
+    private Empleado supervisor;
+
+    @OneToMany(mappedBy = "supervisor")
+    private List<Empleado> subordinados = new ArrayList<>();
+
+    @ManyToOne
+    private Oficina oficina;
+
+    public Empleado(String nombre, String apellidos, String extension, String email, String titulacionEstudiosMaximos, Oficina oficina) {
         this.nombre = nombre;
         this.apellidos = apellidos;
         this.extension = extension;
         this.email = email;
         this.titulacionEstudiosMaximos = titulacionEstudiosMaximos;
+        this.oficina = oficina;
+    }
+
+    public Empleado(String nombre, String apellidos, String extension, String email, String titulacionEstudiosMaximos, Empleado supervisor, List<Empleado> subordinados, Oficina oficina) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.extension = extension;
+        this.email = email;
+        this.titulacionEstudiosMaximos = titulacionEstudiosMaximos;
+        this.supervisor = supervisor;
+        this.subordinados = subordinados;
+        this.oficina = oficina;
+    }
+
+    public Empleado(String nombre, String apellidos, String extension, String email, String titulacionEstudiosMaximos, List<Empleado> subordinados, Oficina oficina) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.extension = extension;
+        this.email = email;
+        this.titulacionEstudiosMaximos = titulacionEstudiosMaximos;
+        this.subordinados = subordinados;
+        this.oficina = oficina;
     }
 
     public void setNombre(String nombre) {
@@ -51,5 +86,38 @@ public class Empleado {
 
     public void setTitulacionEstudiosMaximos(String titulacionEstudiosMaximos) {
         this.titulacionEstudiosMaximos = titulacionEstudiosMaximos;
+    }
+
+    public Oficina getOficina() {
+        return oficina;
+    }
+
+    public void setOficina(Oficina oficina) {
+        this.oficina = oficina;
+    }
+
+    public void asignarSupervisor(Empleado supervisor) {
+        this.supervisor = supervisor;
+        if (supervisor != null && !supervisor.getSubordinados().contains(this)) {
+            supervisor.agregarSubordinado(this);
+        }
+    }
+
+    public void agregarSubordinado(Empleado subordinado) {
+        subordinados.add(subordinado);
+        subordinado.setSupervisor(this);
+    }
+
+    public void removerSubordinado(Empleado subordinado) {
+        subordinados.remove(subordinado);
+        subordinado.setSupervisor(null);
+    }
+
+    public Empleado getSupervisor() {
+        return supervisor;
+    }
+
+    public void setSupervisor(Empleado supervisor) {
+        this.supervisor = supervisor;
     }
 }
---
+++ src/main/java/com/holidevs/ventas/modelos/Oficina.java
@@ -0,0 +1,83 @@
+package com.holidevs.ventas.modelos;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@ToString
+public class Oficina {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ID_Oficina", nullable = false)
+    private Long idOficina;
+
+    private String telefono;
+    private String direccion;
+    private String poblacion;
+    private String provincia;
+    private String codigo_postal;
+    private String pais;
+
+    @OneToMany(mappedBy = "oficina")
+    private List<Empleado> empleados = new ArrayList<>();
+
+    public Oficina(String telefono, String direccion, String poblacion, String provincia, String codigo_postal, String pais) {
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.poblacion = poblacion;
+        this.provincia = provincia;
+        this.codigo_postal = codigo_postal;
+        this.pais = pais;
+    }
+
+    public Oficina(String telefono, String direccion, String poblacion, String provincia, String codigo_postal, String pais, List<Empleado> empleados) {
+        this.telefono = telefono;
+        this.direccion = direccion;
+        this.poblacion = poblacion;
+        this.provincia = provincia;
+        this.codigo_postal = codigo_postal;
+        this.pais = pais;
+        this.empleados = empleados;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public void setPoblacion(String poblacion) {
+        this.poblacion = poblacion;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+
+    public void setCodigo_postal(String codigo_postal) {
+        this.codigo_postal = codigo_postal;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+    public void agregarEmpleado(Empleado empleado) {
+        empleados.add(empleado);
+        empleado.setOficina(this);
+    }
+
+    public void removerEmpleado(Empleado empleado) {
+        empleados.remove(empleado);
+        empleado.setOficina(null);
+    }
+}
